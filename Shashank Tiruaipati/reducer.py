a0c435d24c692a1fd3e8c4216c46a400ff27ac20
Laxmi Sai Teja Naraharasetty <nlsaiteja@gmail.com>
2017-04-14 18:11:17 -0500

    first commit

---
+++ Shashank Tiruaipati/reducer.py
@@ -0,0 +1,25 @@
+sorted = open("jobsort.txt","r")     #reads data from jobsort.txt
+results = open("output.txt", "w")   #writes the result into output.txt
+
+CASE_STATUS = 0        
+JOB_TITLE = None    
+lines = sorted.readlines() #line by line 
+for line in lines:
+    datalist = line.strip().split("\t")
+    if len(datalist) != 2:  # if bad input line
+       continue             # ignore it
+
+    thisjob, thisCase = datalist  # read into variables
+    if JOB_TITLE and JOB_TITLE != thisjob:        
+        results.write("{0}\t{1}\n".format(JOB_TITLE, CASE_STATUS))
+        JOB_TITLE = thisjob;
+        CASE_STATUS = 0
+
+    JOB_TITLE = thisjob        
+    CASE_STATUS += 1   
+
+if JOB_TITLE != None: 
+    results.write("{0}\t{1}\n".format(JOB_TITLE, CASE_STATUS))
+
+sorted.close() 
+results.close() 
